a9b0fd92b0b0c9a077c5d8554250ef0b322021d4
Saibaba161 <kothasaibaba460@gmail.com>
2025-04-06 19:00:19 +0530

    supabase setup

---
+++ src/app/home/page.tsx
@@ -0,0 +1,6 @@
+export default function EmailMarketThing () {
+
+    return (
+        <div>This is home route</div>
+    )
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+
+export default function Login() {
+    return(
+        <div>This is Login route</div>
+    )
+}
